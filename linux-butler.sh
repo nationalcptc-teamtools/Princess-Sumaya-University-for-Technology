d0174563b52b8096739e41cf63e819a29df51fae
Import3r <jamfosa@protonmail.com>
2021-01-05 15:32:04 +0200

    Modify instaollation method of some tools

--- linux-butler.sh
+++ linux-butler.sh
@@ -1,37 +1,33 @@
 #! /bin/bash
 
+# A script that exists for no good reason. I dont know why its a script, maybe I'm bored.
+
 fin_msg(){
 		echo -e "\n\n#################################\n\t$1 DONE\n#################################\n\n"
 	}
+
+# check if it ran before
+[ -d "$HOME/dropzone/github-tools" ] && { echo -e "\n\nDropzone directory exists, maybe you ran this before?"; exit; }
 
 # checks sudo access
 sudo echo || { echo -e "\n\nsudo access denied. run script with a sudoer please."; exit; }
 
 mkdir -p ~/dropzone/github-tools
-
-# Install golang
 cd ~/dropzone
 sudo apt update
-if (which go); then
-	fin_msg 'Go installation' 
-else
-	sudo apt install golang -y
-	fin_msg 'GO installation' 
-fi
+sudo apt install python3 python3-pip seclists curl golang enum4linux gobuster nbtscan nikto nmap onesixtyone oscanner smbclient smbmap smtp-user-enum snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y 
+wait
 
 # Download Tools
 cd ~/dropzone/github-tools
-wget "https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip" && unzip "aquatone_linux_amd64_1.7.0.zip" && fin_msg 'Aquatone' &  # Aquatone
-wget "https://github.com/BloodHoundAD/BloodHound/releases/download/4.0.1/BloodHound-linux-x64.zip" && unzip "BloodHound-linux-x64.zip" && fin_msg 'BloodHound' &  # BloodHound
-git clone https://github.com/Tib3rius/AutoRecon.git && fin_msg 'AutoRecon' &  # AutoRecon
-git clone https://github.com/byt3bl33d3r/CrackMapExec.git & fin_msg 'CrackMapExec' &  # CrackMapExec
+fin_msg 'Gobuster' && fin_msg 'Nikto Scanner' && fin_msg 'Seclists install'
+sudo python3 -m pip install git+https://github.com/Tib3rius/AutoRecon.git && fin_msg 'AutoRecon' &  # AutoRecon
+sudo python3 -m pip install crackmapexec && fin_msg 'CrackMapExec' &  # CrackMapExec
 git clone https://github.com/Joshua1909/smod.git && fin_msg 'SMOD Framework' &  # SMOD framework
-git clone https://github.com/CiscoCXSecurity/enum4linux.git && fin_msg 'enum4linux' &  # enum4linux
 git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git && fin_msg 'linpeas/winpeas' &  # linpeas/winpeas
-git clone https://github.com/OJ/gobuster.git && fin_msg 'Gobuster' &  # Gobuster
-git clone https://github.com/sullo/nikto.git && fin_msg 'Nikto Scanner' &  # Nikto
 git clone https://github.com/openwall/john.git && fin_msg 'John [Jumbo]' &  # John the Ripper Jumbo
+wget "https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip" && unzip "aquatone_linux_amd64_1.7.0.zip" && fin_msg 'Aquatone' &  # Aquatone
 
 wait
-echo -e "\n\nTools pulled successfully.\n\nYou can find them at ~/dropzone/github-tools. Good Luck.\n"
+echo -e "\n\nTools installed successfully.\n\nSome tools are dropped at ~/dropzone/github-tools. Good Luck.\n"
 
