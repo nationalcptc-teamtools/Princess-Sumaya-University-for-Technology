1066a9d93ed138bb9c5f82bb028bc9c2b14a421c
Import3r <jamfosa@protonmail.com>
2021-01-05 17:21:28 +0200

    Add build commands and some fixes

--- fork-spaghetti-installer.sh
+++ fork-spaghetti-installer.sh
@@ -16,18 +16,18 @@ sudo echo || { echo -e "\n\nsudo access denied. run script with a sudoer please.
 mkdir -p ~/dropzone/github-tools
 cd ~/dropzone
 sudo apt update
-sudo apt install python3 python3-pip mc tmux seclists curl golang enum4linux gobuster nbtscan nikto nmap onesixtyone oscanner smbclient smbmap smtp-user-enum snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y 
+sudo apt install python3 python3-pip libssl-dev mc tmux seclists curl golang enum4linux gobuster nbtscan nikto nmap onesixtyone oscanner smbclient smbmap smtp-user-enum snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y 
 wait
 
 # Download Tools
 cd ~/dropzone/github-tools
 fin_msg 'Gobuster' && fin_msg 'Nikto Scanner' && fin_msg 'Seclists install'
-sudo python3 -m pip install git+https://github.com/Tib3rius/AutoRecon.git && fin_msg 'AutoRecon' &  # AutoRecon
 sudo python3 -m pip install crackmapexec && fin_msg 'CrackMapExec' &  # CrackMapExec
+git clone https://github.com/Tib3rius/AutoRecon.git && sudo python3 -m pip install -r ~/dropzone/github-tools/AutoRecon/requirements.txt && fin_msg 'AutoRecon' &  # AutoRecon
 git clone https://github.com/Joshua1909/smod.git && fin_msg 'SMOD Framework' &  # SMOD framework
 git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git && fin_msg 'linpeas/winpeas' &  # linpeas/winpeas
-git clone https://github.com/openwall/john.git && fin_msg 'John [Jumbo]' &  # John the Ripper Jumbo
-wget "https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip" && unzip "aquatone_linux_amd64_1.7.0.zip" && fin_msg 'Aquatone' &  # Aquatone
+git clone https://github.com/openwall/john.git && $(cd ~/dropzone/github-tools/john/src && ./configure && make) && fin_msg 'John [Jumbo]' &  # John the Ripper Jumbo
+wget "https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip" && unzip "aquatone_linux_amd64_1.7.0.zip" -d ./aquatone && fin_msg 'Aquatone' &  # Aquatone
 
 wait
 echo -e "\n\nTools installed successfully.\n\nSome tools are dropped at ~/dropzone/github-tools. Good Luck.\n"
