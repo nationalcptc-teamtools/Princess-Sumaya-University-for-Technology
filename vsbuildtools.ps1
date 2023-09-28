99d231401f3e1045e974fe51341305ae258962ba
ScribblerCoder <omar2001.oh@gmail.com>
2023-09-29 02:37:15 +0300

    smaller build tools package and install git

--- vsbuildtools.ps1
+++ vsbuildtools.ps1
@@ -1,6 +1,5 @@
 # Visual studio 2019 build tools + cpp build tools 
-choco install visualstudio2019buildtools --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US"
-choco install visualstudio2019-workload-vctools --package-parameters "--includeOptional"
+choco install visualstudio2019buildtools visualstudio2019-workload-vctools -y
 
 # add msbuild to path
 [System.Environment]::SetEnvironmentVariable("Path", [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine) + ";C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin", [System.EnvironmentVariableTarget]::Machine)
