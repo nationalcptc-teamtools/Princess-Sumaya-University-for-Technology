5d1d332cccc4eeed3098eeaea76b2f23ade5b761
ScribblerCoder <omar2001.oh@gmail.com>
2023-09-29 00:58:07 +0300

    rename linux script and add windows scripts

---
+++ windows.ps1
@@ -0,0 +1,16 @@
+# exit if not admin
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Write-Warning "This script needs elevated privileges!`nPlease re-run this script as an Administrator!" ; break }
+
+# disable Defender/Firewall because we don't f#$kin care about it 
+Get-MpComputerStatus
+Set-MpPreference -DisableRealtimeMonitoring $true
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+# Install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# windows terminal, openssh and burpsuite
+choco install microsoft-windows-terminal openssh burp-suite-free-edition -y
+
+# snaffler
+Invoke-WebRequest https://github.com/SnaffCon/Snaffler/releases/download/1.0.135/Snaffler.exe -OutFile ~\Desktop\Snaffler.exe
