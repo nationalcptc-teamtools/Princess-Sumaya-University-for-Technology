e55308f259c1156757c52eef9dd41bad4fb55793
Ahmad Obeidat <125534008+ahmdobeidat@users.noreply.github.com>
2025-10-11 18:15:53 +0300

    Add cyberchef to installation script
    Added 'cyberchef' to the installation list in all.sh.

--- all.sh
+++ all.sh
@@ -60,7 +60,8 @@ sudo apt install -y \
     python3.12-venv \
     nuclei \
     dirsearch \
-    flameshot
+    flameshot \
+    cyberchef
 
 wait
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
